bba9bdc8a2fcf3654379c7453d2221ff7bc0b16c
WolfBrin14 <wolfsaffran@brinpartners.com>
2024-10-18 23:02:50 -0300

    Corrigir renderização do componente para o lado do cliente

--- jogo-da-forca-unicornio/app/page.tsx
+++ jogo-da-forca-unicornio/app/page.tsx
@@ -1,4 +1,8 @@
-import JogoDaForcaUnicornio from './JogoDaForcaUnicornio';
+'use client';
+
+import dynamic from 'next/dynamic'
+
+const JogoDaForcaUnicornio = dynamic(() => import('./JogoDaForcaUnicornio'), { ssr: false })
 
 export default function Home() {
   return <JogoDaForcaUnicornio />;
